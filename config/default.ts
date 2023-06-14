1296b8e67025fda0051e4987ac03320348dc162e
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-06-14 21:53:52 +0545

    Kept isDevEnv in config & used http-status-code

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,6 @@ import path from 'path';
 
 export const basePath = path.join(__dirname, '..');
 export const isDevEnv = process.env.NODE_ENV === 'development';
-
 export default {
   basePath,
   db: {
