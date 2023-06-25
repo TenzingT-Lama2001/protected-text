6b9617c099169c894ccf29bdb4d4d7ed76214dbf
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-06-25 17:41:29 +0545

    put all the route methods in the constructor

---
+++ src/route/v1/index.ts
@@ -0,0 +1,12 @@
+import express, { Router } from 'express';
+import { NoteRoutes } from './notes/index';
+import { EncryptionRoutes } from './encryption';
+
+export class V1Routes {
+  public router: Router = express.Router();
+
+  constructor() {
+    this.router.use('/notes', new NoteRoutes().router);
+    this.router.use('/notes', new EncryptionRoutes().router);
+  }
+}
