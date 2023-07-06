a8843a23c9fdf427e2dfa7ac0fd9301d57808ca1
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-06 20:34:57 +0545

    Update routes with middleware

--- src/route/v1/encryption/index.ts
+++ src/route/v1/encryption/index.ts
@@ -1,12 +1,22 @@
 import express, { Router } from 'express';
 import asyncHandler from 'express-async-handler';
 import { EncryptionController } from 'src/controller/encryption.controller';
+import validateSchema from 'src/middleware/validation.middleware';
+import encryptionSchema from 'src/schema/encryption.schema';
 
 export class EncryptionRoutes {
   public router: Router = express.Router();
 
   constructor() {
-    this.router.post('/encrypt', asyncHandler(EncryptionController.encrypt));
-    this.router.post('/decrypt', asyncHandler(EncryptionController.decrypt));
+    this.router.post(
+      '/encrypt',
+      validateSchema(encryptionSchema.encrypt, 'body'),
+      asyncHandler(EncryptionController.encrypt),
+    );
+    this.router.post(
+      '/decrypt',
+      validateSchema(encryptionSchema.decrypt, 'body'),
+      asyncHandler(EncryptionController.decrypt),
+    );
   }
 }
--- src/route/v1/notes/index.ts
+++ src/route/v1/notes/index.ts
@@ -1,14 +1,16 @@
 import express, { Router } from 'express';
 import { NoteController } from 'src/controller/note.controller';
 import asyncHandler from 'express-async-handler';
+import validateSchema from 'src/middleware/validation.middleware';
+import notesSchema from 'src/schema/note.schema';
 
 export class NoteRoutes {
   public router: Router = express.Router();
 
   constructor() {
-    this.router.get('/:id', asyncHandler(NoteController.getNote));
-    this.router.post('/', asyncHandler(NoteController.postNote));
-    this.router.delete('/:id', asyncHandler(NoteController.deleteNote));
-    this.router.patch('/:id', asyncHandler(NoteController.updateNote));
+    this.router.get('/:id', validateSchema(notesSchema.get, 'params'), asyncHandler(NoteController.getNote));
+    this.router.post('/', validateSchema(notesSchema.post, 'body'), asyncHandler(NoteController.postNote));
+    this.router.delete('/:id', validateSchema(notesSchema.delete, 'query'), asyncHandler(NoteController.deleteNote));
+    this.router.patch('/:id', validateSchema(notesSchema.patch, 'body'), asyncHandler(NoteController.updateNote));
   }
 }
