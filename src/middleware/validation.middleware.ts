4286a0847cddb31553fdb81a4b85690aa0082ef2
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-18 20:07:37 +0545

    Update validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,24 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import { validationResult, ValidationChain } from 'express-validator';
+import asyncHandler from 'express-async-handler';
+import HTTP_STATUS from 'http-status-codes';
 
 const validateSchema = (schema: ValidationChain[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await Promise.all(schema.map((validation) => validation.run(req)));
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        const errorMsg = errors
-          .array()
-          .map((err) => err.msg)
-          .toString();
+  return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const validationPromises = schema.map((validation) => validation.run(req));
+    const validationResults = await Promise.all(validationPromises);
 
-        res.status(400);
-        throw new Error(errorMsg);
-      }
-      next();
-    } catch (error) {
-      next(error);
+    const hasErrors = validationResults.some((result) => !result.isEmpty());
+    if (hasErrors) {
+      const errors = validationResult(req);
+      const errorMsg = errors
+        .array()
+        .map((err) => err.msg)
+        .toString();
+      res.status(HTTP_STATUS.BAD_REQUEST);
+      throw new Error(errorMsg);
     }
-  };
+
+    next();
+  });
 };
 export default validateSchema;
