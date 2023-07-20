3a226ac936cf032becc9ba27d0d94337ca51f152
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-20 09:58:13 +0545

    Update note route

--- src/route/v1/note/index.ts
+++ src/route/v1/note/index.ts
@@ -2,15 +2,15 @@ import express, { Router } from 'express';
 import { NoteController } from 'src/controller/note.controller';
 import asyncHandler from 'express-async-handler';
 import validateSchema from 'src/middleware/validation.middleware';
-import { notesDeleteSchema, notesGetSchema, notesPatchSchema, notesPostSchema } from 'src/schema/note.schema';
+import { notesPatchSchema, notesPostSchema } from 'src/schema/note.schema';
 
 export class NoteRoutes {
   public router: Router = express.Router();
 
   constructor() {
-    this.router.get('/:id', validateSchema(notesGetSchema), asyncHandler(NoteController.getNote));
+    this.router.get('/:id', asyncHandler(NoteController.getNote));
     this.router.post('/', validateSchema(notesPostSchema), asyncHandler(NoteController.postNote));
-    this.router.delete('/:id', validateSchema(notesDeleteSchema), asyncHandler(NoteController.deleteNote));
+    this.router.delete('/:id', asyncHandler(NoteController.deleteNote));
     this.router.patch('/:id', validateSchema(notesPatchSchema), asyncHandler(NoteController.updateNote));
   }
 }
