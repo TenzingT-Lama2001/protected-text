f06dbdad285b960e6ee205908a749d7f95c78342
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-24 00:22:01 +0545

    Throwing custom error in validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { validationResult, ValidationChain } from 'express-validator';
 import HTTP_STATUS from 'http-status-codes';
+import asyncHandler from 'express-async-handler';
+import { CustomError } from 'src/error/custom.error';
 
 const validateSchema = (schema: ValidationChain[]) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
+  return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const promises = [];
     for (let i = 0; i < schema.length; i += 1) {
       promises.push(schema[i].run(req));
@@ -11,14 +13,10 @@ const validateSchema = (schema: ValidationChain[]) => {
     await Promise.all(promises);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const errorMsg = errors
-        .array()
-        .map((err) => err.msg)
-        .toString();
-      res.status(HTTP_STATUS.BAD_REQUEST);
-      return next(new Error(errorMsg));
+      const errorDetails = errors.array() as [];
+      throw new CustomError('Validation Error', HTTP_STATUS.BAD_REQUEST, errorDetails);
     }
     return next();
-  };
+  });
 };
 export default validateSchema;
