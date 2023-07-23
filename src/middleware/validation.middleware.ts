020bef61454188071718788168ef208d5b2300d7
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-24 00:37:35 +0545

    throwing message and cause from constant

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -3,6 +3,7 @@ import { validationResult, ValidationChain } from 'express-validator';
 import HTTP_STATUS from 'http-status-codes';
 import asyncHandler from 'express-async-handler';
 import { CustomError } from 'src/error/custom.error';
+import { errorList } from 'src/error/constant';
 
 const validateSchema = (schema: ValidationChain[]) => {
   return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
@@ -14,7 +15,12 @@ const validateSchema = (schema: ValidationChain[]) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const errorDetails = errors.array() as [];
-      throw new CustomError('Validation Error', HTTP_STATUS.BAD_REQUEST, errorDetails);
+      throw new CustomError(
+        errorList.validationError.message,
+        HTTP_STATUS.BAD_REQUEST,
+        errorDetails,
+        errorList.validationError.cause,
+      );
     }
     return next();
   });
