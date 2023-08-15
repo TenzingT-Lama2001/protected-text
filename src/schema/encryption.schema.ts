b6c6898fc7b2c30820eeff7dfbc32722351ef903
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-08-15 14:23:22 +0545

    Add key in encryption schema

--- src/schema/encryption.schema.ts
+++ src/schema/encryption.schema.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 const encryptionDecryptionSchema = [
   body('note').notEmpty().withMessage('Note is required'),
   body('secretKey').notEmpty().withMessage('Secret Key is required'),
+  body('key').notEmpty().withMessage('Key is required'),
 ];
 
 export { encryptionDecryptionSchema };
