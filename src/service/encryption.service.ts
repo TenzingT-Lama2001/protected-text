1c161766fba085233a156338b566db41937c905d
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-08-15 14:24:00 +0545

    Update encryption mechanism in the service

--- src/service/encryption.service.ts
+++ src/service/encryption.service.ts
@@ -1,18 +1,46 @@
 import CryptoJS from 'crypto-js';
 
+export type TDecryptNote = {
+  success: boolean;
+  decryptedNote?: string;
+};
+
 export class EncryptionService {
-  public static encrypt(note: string, secretKey: string): string {
-    const encryptedNote = CryptoJS.AES.encrypt(note, secretKey).toString();
+  public static encrypt(note: string, secretKey: string, keyHash: string): string {
+    // encrypt(content + keyHash, password)
+    const encryptedNote = CryptoJS.AES.encrypt(String(note + keyHash), secretKey).toString();
     return encryptedNote;
   }
 
-  public static decrypt(note: string, secretKey: string): string {
-    const decryptedText = CryptoJS.AES.decrypt(note, secretKey).toString(CryptoJS.enc.Utf8);
-    return decryptedText;
+  public static decrypt(encryptedNote: string, secretKey: string, keyHash: string): TDecryptNote {
+    let decryptedContent = '';
+    let decryptedNote = '';
+
+    try {
+      decryptedContent = CryptoJS.AES.decrypt(encryptedNote, secretKey).toString(CryptoJS.enc.Utf8);
+      if (decryptedContent.indexOf(keyHash, decryptedContent.length - keyHash.length) !== -1) {
+        decryptedNote = decryptedContent.substring(0, decryptedContent.length - keyHash.length);
+        return {
+          success: true,
+          decryptedNote,
+        };
+      }
+    } catch (err) {
+      return {
+        success: false,
+      };
+    }
+    return {
+      success: false,
+    };
   }
 
-  public static hash(note: string): string {
-    const hash = CryptoJS.SHA512(note).toString();
+  public static hash(payload: string): string {
+    const hash = CryptoJS.SHA512(payload).toString();
     return hash;
+  }
+
+  public static hashContent(note: string, secretKey: string, dbVersion = 2) {
+    return CryptoJS.SHA512(note + CryptoJS.SHA512(secretKey).toString()).toString() + dbVersion;
   }
 }
