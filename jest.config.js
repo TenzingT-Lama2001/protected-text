4bc016676c660fbf08e2b4162388a0bda585ebd8
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-08-21 10:56:39 +0545

    Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  clearMocks: true,
+  coveragePathIgnorePatterns: ['/node_modules'],
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['ts', 'js'],
+  roots: ['<rootDir>/src'],
+};
