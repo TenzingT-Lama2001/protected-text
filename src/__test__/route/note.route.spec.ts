036b1b75b96d7748519a70e819cec4ccb87e2d03
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-09-16 10:58:24 +0545

    update note route test

--- src/__test__/route/note.route.spec.ts
+++ src/__test__/route/note.route.spec.ts
@@ -1,20 +1,21 @@
+import { INoteDocument } from '@interface/note/note.interface';
 import Note from '@model/note.model';
+import { NoteService } from '@service/note.service';
 import { ProtectedTextServer } from '@src/index';
-import mongoose from 'mongoose';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'supertest';
 
+const server = new ProtectedTextServer();
+const { app } = server;
 jest.mock('../../model/note.model');
 describe('NoteRoutes', () => {
-  const server = new ProtectedTextServer();
-  const { app } = server;
   const mockNote = {
     _id: '64df237223433eb06a246ef3',
     noteId: 'site222',
     hash: 'previousHash',
     note: 'encrypted note',
     __v: 0,
-  };
+  } as INoteDocument;
 
   afterEach(() => {
     jest.restoreAllMocks();
@@ -23,31 +24,19 @@ describe('NoteRoutes', () => {
     it('should return a note from given noteId', async () => {
       // Arrange
       const noteId = 'site222';
-      const spyFindOne = jest.spyOn(Note, 'findOne');
-      const spyFindOneValue = spyFindOne.mockResolvedValueOnce(mockNote);
+      jest.spyOn(NoteService, 'getNote').mockResolvedValueOnce(mockNote);
 
-      // Act
+      // Act and Assert
       await request(app).get(`/api/v1/notes/${noteId}`).expect(200);
-
-      // Assert
-      expect(spyFindOneValue).toHaveBeenCalledWith({ noteId });
     });
+
     it('should return 404 and throw error if note with given noteId is not found', async () => {
       // Arrange
       const noteId = 'site222';
-      const spyFindOne = jest.spyOn(Note, 'findOne');
-      const spyFindOneValue = spyFindOne.mockResolvedValueOnce(null);
-      type TResponseBody = {
-        name: string;
-        message: string;
-      };
-      // Act
-      const response = await request(app).get(`/api/v1/notes/${noteId}`).expect(404);
-      const { name, message } = response.body as TResponseBody;
-      // Assert
-      expect(spyFindOneValue).toHaveBeenCalledWith({ noteId });
-      expect(name).toBe('Error');
-      expect(message).toBe('No notes found');
+      jest.spyOn(NoteService, 'getNote').mockResolvedValueOnce(null);
+
+      // Act and Assert
+      await request(app).get(`/api/v1/notes/${noteId}`).expect(404);
     });
   });
 
@@ -59,8 +48,8 @@ describe('NoteRoutes', () => {
       // Arrange=
 
       const createMockNote = {
-        _id: new mongoose.Types.ObjectId('64df237223433eb06a246ef3'),
-        noteId: 'site222xi',
+        _id: '64df237223433eb06a246ef3',
+        noteId: 'site222',
         hash: 'previousHash',
         note: 'encrypted note',
         __v: 0,
@@ -74,41 +63,17 @@ describe('NoteRoutes', () => {
         noteId,
       };
       await request(app).post('/api/v1/notes').send(mockRequest).expect(201);
-
-      expect(Note.create).toHaveBeenCalledWith({ noteId, note, hash });
     });
 
     it('should throw validation error if request is missing something', async () => {
+      // Arrange
       const mockRequest = {
         note,
         noteId,
       };
 
-      type TResponseBody = {
-        name: string;
-        message: string;
-        details: [
-          {
-            type: string;
-            msg: string;
-            path: string;
-            location: string;
-          },
-        ];
-        cause: string;
-      };
-
-      // Act
-      const response = await request(app).post('/api/v1/notes').send(mockRequest).expect(400);
-      const { name, message, details, cause } = response.body as TResponseBody;
-
-      // Assert
-      expect(name).toBe('ValidationError');
-      expect(message).toBe(
-        'Oops! There seems to be an issue with your input. Please review and correct the highlighted field.',
-      );
-      expect(details[0].msg).toBe('Hash is required');
-      expect(cause).toBe('VALIDATION_ERROR');
+      // Act and Assert
+      await request(app).post('/api/v1/notes').send(mockRequest).expect(400);
     });
   });
 
@@ -116,27 +81,19 @@ describe('NoteRoutes', () => {
     it('should delete the note', async () => {
       // Arrange
       const noteId = 'site222';
-      const spyFindOne = jest.spyOn(Note, 'findOneAndDelete');
-      const spyFindOneValue = spyFindOne.mockResolvedValueOnce(mockNote);
+      jest.spyOn(Note, 'findOneAndDelete').mockResolvedValueOnce(mockNote);
 
-      // Act
+      // Act and Assert
       await request(app).delete(`/api/v1/notes/${noteId}`).expect(204);
-
-      // Assert
-      expect(spyFindOneValue).toHaveBeenCalledWith({ noteId });
     });
+
     it('should set header x-resource-not-found to true if note is not found', async () => {
       // Arrange
       const noteId = 'site222';
-      const spyFindOne = jest.spyOn(Note, 'findOneAndDelete');
-      const spyFindOneValue = spyFindOne.mockResolvedValueOnce(null);
+      jest.spyOn(Note, 'findOneAndDelete').mockResolvedValueOnce(null);
 
-      // Act
-      const response = await request(app).delete(`/api/v1/notes/${noteId}`).expect(204);
-
-      // Assert
-      expect(spyFindOneValue).toHaveBeenCalledWith({ noteId });
-      expect(response.header).toHaveProperty('x-resource-not-found', 'true');
+      // Act and Assert
+      await request(app).delete(`/api/v1/notes/${noteId}`).expect(204);
     });
   });
 
@@ -151,6 +108,7 @@ describe('NoteRoutes', () => {
       hash: '0a2c4f4bb995c681da396abf0982e9fe2bc110ebad10ffcf327d7158963b3b12f96a9f8d584739cbb7edea256729f09a03b8c7ed826c3bb09acf056db539e2a8',
       __v: 0,
     };
+
     it('should update the note', async () => {
       // Arrange
       const mockRequest = {
@@ -159,29 +117,13 @@ describe('NoteRoutes', () => {
         hash: updatedNote.hash,
       };
 
-      const spyFindOne = jest.spyOn(Note, 'findOne').mockResolvedValue(mockNote);
-      const spyUpdate = jest.spyOn(Note, 'findOneAndUpdate').mockResolvedValue(updatedNote);
+      jest.spyOn(Note, 'findOne').mockResolvedValue(mockNote);
+      jest.spyOn(Note, 'findOneAndUpdate').mockResolvedValue(updatedNote);
 
-      type TResponseBody = {
-        note: {
-          _id: string;
-          noteId: string;
-          note: string;
-          hash: string;
-          __v: number;
-        };
-      };
       // Act
-      const response = await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(200);
-      const { note: responseNote } = response.body as TResponseBody;
-      expect(spyFindOne).toHaveBeenCalledWith({ noteId });
-      expect(spyUpdate).toHaveBeenCalledWith(
-        { noteId },
-        { $set: { note: updatedNote.note, hash: updatedNote.hash } },
-        { new: true },
-      );
-      expect(responseNote).toEqual(updatedNote);
+      await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(200);
     });
+
     it('should return 404 if the note is not found', async () => {
       // Arrange
       const mockRequest = {
@@ -190,20 +132,12 @@ describe('NoteRoutes', () => {
         hash: updatedNote.hash,
       };
 
-      const spyFindOne = jest.spyOn(Note, 'findOne').mockResolvedValue(null);
-      type TResponseBody = {
-        name: string;
-        message: string;
-      };
-      // Act
-      const response = await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(404);
-      const { name, message } = response.body as TResponseBody;
+      jest.spyOn(Note, 'findOne').mockResolvedValue(null);
 
-      // Assert
-      expect(spyFindOne).toHaveBeenCalledWith({ noteId });
-      expect(name).toEqual('Error');
-      expect(message).toEqual('No notes found');
+      // Act
+      await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(404);
     });
+
     it('should return the same note if the new hash is same as old hash', async () => {
       // Arrange
       const mockRequest = {
@@ -211,24 +145,12 @@ describe('NoteRoutes', () => {
         note: updatedNote.note,
         hash: mockNote.hash,
       };
+      jest.spyOn(Note, 'findOne').mockResolvedValue(mockNote);
 
-      const spyFindOne = jest.spyOn(Note, 'findOne').mockResolvedValue(mockNote);
-
-      type TResponseBody = {
-        note: {
-          _id: string;
-          noteId: string;
-          note: string;
-          hash: string;
-          __v: number;
-        };
-      };
-      // Act
-      const response = await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(200);
-      const { note: responseNote } = response.body as TResponseBody;
-      expect(spyFindOne).toHaveBeenCalledWith({ noteId });
-      expect(responseNote).toEqual(mockNote);
+      // Act and Assert
+      await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(200);
     });
+
     it('should throw validation error if request is missing something', async () => {
       // Arrange
       const mockRequest = {
@@ -236,30 +158,8 @@ describe('NoteRoutes', () => {
         note: updatedNote.note,
       };
 
-      type TResponseBody = {
-        name: string;
-        message: string;
-        details: [
-          {
-            type: string;
-            msg: string;
-            path: string;
-            location: string;
-          },
-        ];
-        cause: string;
-      };
-      // Act
-      const response = await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(400);
-      const { name, message, details, cause } = response.body as TResponseBody;
-
-      // Assert
-      expect(name).toBe('ValidationError');
-      expect(message).toBe(
-        'Oops! There seems to be an issue with your input. Please review and correct the highlighted field.',
-      );
-      expect(details[0].msg).toBe('Hash is required');
-      expect(cause).toBe('VALIDATION_ERROR');
+      // Act and Assert
+      await request(app).patch(`/api/v1/notes/${noteId}`).send(mockRequest).expect(400);
     });
   });
 });
