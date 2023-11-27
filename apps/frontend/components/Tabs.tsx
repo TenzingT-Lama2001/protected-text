fe43b1fc233153126b2d7094e996eefa780f88f3
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-11-27 19:05:47 +0545

    Add: Undo timer

--- apps/frontend/components/Tabs.tsx
+++ apps/frontend/components/Tabs.tsx
@@ -15,7 +15,8 @@ function TextTabs() {
   const [tabs, setTabs] = useState<Tab[]>([{ id: generateUuid(), content: '', name: 'Empty Tab' }]);
   const [activeTab, setActiveTab] = useState<string | null>(tabs[0]?.id);
   const setContent = useBoundStore((state) => state.setContent);
-
+  const [closedTab, setClosedTab] = useState<Tab | undefined>();
+  const [undoTimer, setUndoTimer] = useState<number | null>(null);
   const addTab = () => {
     const newTabId = generateUuid();
     const newTab: Tab = { id: newTabId, content: '', name: 'Empty Tab' };
@@ -40,12 +41,33 @@ function TextTabs() {
     if (tabs.length === 1) {
       return;
     }
+    const tabClosed = tabs.find((tab) => tab.id === tabId);
     const updatedTabs: Tab[] = tabs.filter((tab) => tab.id !== tabId);
     const newActiveTab: Tab | undefined = updatedTabs[updatedTabs.length - 1];
     setTabs(updatedTabs);
+    setClosedTab(tabClosed);
+    setUndoTimer(5);
 
     if (activeTab === tabId) {
       setActiveTab(newActiveTab ? newActiveTab.id : null);
+    }
+    // Start a countdown to clear the closedTab and undoTimer states after 5 seconds
+    const countdownInterval = setInterval(() => {
+      setUndoTimer((prevTimer) => (prevTimer ? prevTimer - 1 : null));
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(countdownInterval);
+      setClosedTab(undefined);
+      setUndoTimer(null);
+    }, 5000);
+  };
+
+  const undoCloseTab = () => {
+    if (closedTab) {
+      setTabs((prevTabs) => [...prevTabs, closedTab]);
+      setActiveTab(closedTab.id);
+      setClosedTab(undefined);
     }
   };
   const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -63,14 +85,22 @@ function TextTabs() {
   const renderTabs = () =>
     tabs.map((tab) => (
       <div key={tab.id} className="flex space-x-1 mr-1 text-xs">
-        <div className={`relative flex  px-1 mr-1  ${tab.id === activeTab ? 'text-gray bg-white' : 'bg-gray-200'}`}>
-          <button type="button" onClick={() => setActiveTab(tab.id)} className="text-xs p-1 rounded-md h-full mr-1">
+        <div
+          className={`flex items-center px-2 rounded-md border border-gray-200 ${
+            tab.id === activeTab ? 'bg-white' : 'bg-gray-100'
+          }`}
+        >
+          <button
+            type="button"
+            onClick={() => setActiveTab(tab.id)}
+            className={`text-xs p-1  ${tab.id === activeTab ? 'text-gray-700' : 'text-gray-500'}`}
+          >
             {tab.name}
           </button>
           <button
             type="button"
             onClick={() => closeTab(tab.id)}
-            className="font-semibold absolute top-0 right-0 text-gray-700"
+            className="text-gray-500 hover:text-gray-700 ml-2 font-bold"
           >
             X
           </button>
@@ -79,22 +109,41 @@ function TextTabs() {
     ));
 
   return (
-    <div className="w-[calc(100% - 2rem)] max-w-screen-xl mx-auto bg-[#F4F4F5]">
+    <div className="w-[calc(100% - 2rem)] max-w-screen-xl mx-auto bg-[#222222] border rounded-md border-gray-700">
       <div className="flex flex-wrap items-center mb-1">
         {renderTabs()}
         <button
           type="button"
           onClick={() => addTab()}
-          className="bg-[#4F46E5] text-white rounded-sm hover:bg-[#6057ff] flex items-center h-4 w-4 text-center justify-center"
+          className="bg-[#33996B] text-white rounded-full hover:bg-[#70b395] w-5 h-5 flex justify-center items-center border-2 border-[#33996B] shadow-md p-2"
         >
-          +
+          <span className="text-sm">+</span>
         </button>
+      </div>
+
+      <div className="flex items-center text-sm text-gray-500 mt-2">
+        {closedTab && (
+          <span className="p-1">
+            Tab &ldquo;{closedTab.name}&rdquo; closed.{' '}
+            {undoTimer !== null && (
+              <>
+                <button type="button" onClick={() => undoCloseTab()} className="underline cursor-pointer">
+                  Undo
+                </button>{' '}
+                in {undoTimer} seconds.{' '}
+              </>
+            )}
+          </span>
+        )}
       </div>
 
       <textarea
         value={tabs.find((tab) => tab.id === activeTab)?.content}
+        placeholder="your text goes here..."
         onChange={(e) => handleTextAreaChange(e)}
-        className="w-full min-h-[28rem] p-2 border border-gray-300 rounded resize-vertical"
+        className="w-full min-h-[32rem] p-2 border-1  rounded resize-vertical bg-[#222222] text-[#D6D6D6]
+        focus:outline-none
+        "
       />
     </div>
   );
