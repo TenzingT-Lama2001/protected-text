409c6a6fb0ce52cad636d9d2e261c5fb12053848
TenzingT-Lama2001 <52858291+TenzingT-Lama2001@users.noreply.github.com>
2023-12-14 19:39:07 +0545

    Feature/pt 42/UI implementation (#44)
    * Add: Logo for protected-text
    
    * Add: React icons for "lock" icon
    
    * Add: Undo timer
    
    * Update: Minor style changes
    
    * Add: Landing Page

--- apps/frontend/components/Modal.tsx
+++ apps/frontend/components/Modal.tsx
@@ -18,26 +18,26 @@ export default function ConfirmationModal({ onConfirm }: ConfirmationModalProps)
   };
 
   return (
-    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-md shadow-md sm:w-96 w-[calc(100%-2rem)] mx-auto">
-      <div className="flex flex-col flex-wrap gap-3">
-        <p className="text-lg md:text-xl font-semibold">Create new site?</p>
-        <p className="text-base md:text-lg">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#242424] p-4 rounded-md shadow-md sm:w-96 w-[calc(100%-2rem)] mx-auto">
+      <div className="flex flex-col flex-wrap gap-1 p-2">
+        <p className="text-lg md:text-xl font-normal text-[#D9D9D9]">Create new site?</p>
+        <p className="md:text-lg font-normal text-[#7E7E7E]">
           Great! This site doesn&rsquo;t exist, it can be yours! Would you like to create:
         </p>
       </div>
 
-      <div className="flex justify-center space-x-4 mt-4">
+      <div className="flex justify-center space-x-4 mt-3">
         <button
           type="button"
           onClick={handleCreateSite}
-          className="px-4 py-2 md:px-6 md:py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          className="text-[#ffffff] bg-[#33996B] hover:bg-[#70b395] rounded-md py-2.5 px-3"
         >
           Create site
         </button>
         <button
           type="button"
           onClick={handleNoClick}
-          className="px-4 py-2 md:px-6 md:py-3 bg-red-500 text-white rounded-md hover:bg-red-600"
+          className="text-[#ffffff] bg-[#af3131] hover:bg-[#c24c4c] rounded-md py-2.5 px-5 "
         >
           No
         </button>
--- apps/frontend/components/NavBar.tsx
+++ apps/frontend/components/NavBar.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { useBoundStore } from '../store/store';
+import { Logo } from './Logo';
 
 export default function NavBar() {
   const content = useBoundStore((state) => state.content);
@@ -8,26 +8,33 @@ export default function NavBar() {
   };
   return (
     <header>
-      <nav className="bg-[#4F46E5] text-white flex flex-col justify-center items-center p-3 ">
-        <Link href="/" className="text-[24px] font-medium">
-          ProtectedText
-        </Link>
-
-        <div className="flex gap-3 text-sm my-2 justify-between sm:gap-4">
-          <button type="button" className="text-[#4F46E5] bg-white p-1 rounded-md sm:px-2 py-1">
+      <nav className="bg-[#1C1C1C] text-white flex flex-col justify-center items-center p-2  border-b border-gray-600 ">
+        <Logo />
+        <div className="flex gap-3 text-sm my-1 justify-between sm:gap-3">
+          <button
+            type="button"
+            className="text-[#33996B] bg-gray-100 border-[#33996B] p-1 rounded-sm py-2 px-1.5 sm:py-2 sm:px-4 sm:rounded-md transition-all hover:bg-white hover:text-[#70b395]"
+          >
             Reload
           </button>
+
           <button
             onClick={() => handleOnSave()}
             type="button"
-            className="text-[#4F46E5] bg-white p-1 rounded-md sm:px-2 py-1"
+            className="text-[#ffffff] bg-[#33996B] p-1 hover:bg-[#70b395] rounded-sm py-2 px-1.5  sm:py-2 sm:px-4 sm:rounded-md"
           >
             Save
           </button>
-          <button type="button" className="text-[#4F46E5] bg-white p-1 rounded-md sm:px-2 py-1">
-            Change Password
+          <button
+            type="button"
+            className="text-[#af3131] border-[#33996B] bg-gray-100 p-1 hover:bg-white hover:text-[#c24c4c] rounded-sm py-2 px-1.5  sm:py-2 sm:px-4 sm:rounded-md"
+          >
+            Reset Password
           </button>
-          <button type="button" className="text-[#4F46E5] bg-white p-1 rounded-md sm:px-2 py-1">
+          <button
+            type="button"
+            className="text-[#ffffff] bg-[#af3131] p-1 hover:bg-[#c24c4c] rounded-sm py-2 px-1.5 sm:py-2 sm:px-4 sm:rounded-md"
+          >
             Delete
           </button>
         </div>
