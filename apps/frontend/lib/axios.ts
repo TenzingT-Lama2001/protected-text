c9af9ce03904935a66d8503335a98a0f729a1369
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-12-29 19:23:54 +0545

    Update: api url

--- apps/frontend/lib/axios.ts
+++ apps/frontend/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const baseURL = 'http://localhost:3001/api';
+const baseURL = 'https://protected.tenzing121.com.np';
 const apiVersion = 'v1';
 
 const createAxiosInstance = (apiV: string) =>
